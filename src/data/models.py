2b23f9fe3cbf6ae78ce9053dc689b4ee61b3d4bf
bouduben <bouduben@gmail.com>
2022-12-08 14:29:29 +0100

    update api sort with db sort nv0

--- src/data/models.py
+++ src/data/models.py
@@ -265,28 +265,32 @@ class Sort(BaseModel):
         ('distance', 'distance'),
         (None, 'aucune'),
     ]
-
+    niveaux_superieurs =  composante_desc = models.TextField(
+        'description aux niveaux supèrieur', null=True, blank=True)
     niveau = models.IntegerField(default=1)
     ecole = models.ForeignKey(
         'EcoleMagie', blank=True, null=True, related_name='sort', on_delete=models.CASCADE)
-    temps_incantation = models.ForeignKey(
-        'Periode', blank=True, related_name='temps_incantation', on_delete=models.CASCADE)
     rituel = models.BooleanField(default=False)
     concentration = models.BooleanField(default=False)
-    duree = models.ForeignKey(
-        'Periode', blank=True, related_name='duree_sort', on_delete=models.CASCADE)
-    portee = models.ForeignKey(
-        'Portee', blank=True, null=True, on_delete=models.CASCADE)
+    temps_incantation = models.CharField(
+        "temps d'incantation", blank=True, default='1 action', max_length=50)
+    duree = models.CharField(
+        "durée du sort", null=True, blank=True, default=None, max_length=50)
+    portee = models.CharField(
+        "portée du sort", null=True, blank=True, default=None, max_length=50)
     composantes = models.ManyToManyField('Composante', blank=True)
     composante_desc = models.TextField(
         'composantes description', null=True, blank=True)
     jets_sauvegardes = models.ForeignKey(
         'Caracteristique', null=True, blank=True, related_name='sort', on_delete=models.CASCADE)
+    reussite_JdS = models.CharField(
+        "réussite JdS", null=True, blank=True, default=None, max_length=50)
     jet_attaque = models.CharField(
         "jets d'attaque", null=True, blank=True, default=None, max_length=50, choices=CHOIX_ATTAQUE)
-    degats = models.ManyToManyField('DegatSort', blank=True)
-    cible = models.ForeignKey(
-        'Cible', null=True, blank=True, related_name='sort', on_delete=models.CASCADE)
+    degats_par_niveaux = models.ForeignKey('DegatSortNiveaux',null=True, blank=True, on_delete=models.CASCADE)
+    degats_par_emplacement = models.ForeignKey('DegatSortEmplacements',null=True, blank=True, on_delete=models.CASCADE)
+    soins_par_emplacement = models.ForeignKey('SoinSortEmplacements',null=True, blank=True, on_delete=models.CASCADE)
+
 
 
 class EcoleMagie(BaseModel):
@@ -492,17 +496,6 @@ class Cible(models.Model):
         else:
             return f'{self.cible} {self.type}'
 
-class Periode(models.Model):
-    quantite = models.IntegerField(default=1)
-    mesure = models.CharField(null=True, blank=True, max_length=50)
-
-    def __str__(self):
-        if self.quantite == 0:
-            return 'instantanée'
-        if self.quantite > 1:
-            return f'{self.quantite} {self.mesure}s'
-        else:
-            return f'{self.quantite} {self.mesure}'
 
 class Composante(BaseModel):
     abreviation = models.CharField(null=True, blank=True, max_length=50)
@@ -609,18 +602,66 @@ class Degat(models.Model):
     type = models.ForeignKey("TypeDegat", blank=True,
                              null=True, on_delete=models.CASCADE)
 
+    class Meta:
+        ordering = ['type']
+
     def __str__(self):
         if self.bonus == 0:
             return f'{self.de}({self.type.index})'
         return f'{self.de}+{self.bonus}({self.type.index})'
 
 
-class DegatSort(Degat):
-    desc = models.TextField(null=True, blank=True)
-    JdS_condition = models.ForeignKey(
-        'Caracteristique', null=True, blank=True, related_name='degat_de_sort', on_delete=models.CASCADE)
-    par_niveau = models.BooleanField('dégâts/niveau', default=False)
+class Soin(models.Model):
+    bonus = models.IntegerField(default=0)
+    bonus_mod = models.BooleanField(default=False)
+    de = models.CharField('dé de dégat', default='1d6', max_length=50)
+    type = models.ForeignKey("TypeDegat", blank=True,
+                             null=True, on_delete=models.CASCADE)
 
+    def __str__(self):
+        if self.bonus == 0:
+            if self.bonus_mod:
+                return f'{self.de}({self.type.index}) + MOD'
+        if self.bonus_mod:
+            return f'{self.de}+{self.bonus}({self.type.index}) + MOD'
+        return f'{self.de}+{self.bonus}({self.type.index})'
+
+
+class DegatSortEmplacements(models.Model):
+    desc = models.TextField(null=True, blank=True)
+    emplacement_nv0 = models.ManyToManyField('Degat',blank=True,related_name='emplacement_nv0')
+    emplacement_nv1 = models.ManyToManyField('Degat',blank=True,related_name='emplacement_nv1')
+    emplacement_nv2 = models.ManyToManyField('Degat',blank=True,related_name='emplacement_nv2')
+    emplacement_nv3 = models.ManyToManyField('Degat',blank=True,related_name='emplacement_nv3')
+    emplacement_nv4 = models.ManyToManyField('Degat',blank=True,related_name='emplacement_nv4')
+    emplacement_nv5 = models.ManyToManyField('Degat',blank=True,related_name='emplacement_nv5')
+    emplacement_nv6 = models.ManyToManyField('Degat',blank=True,related_name='emplacement_nv6')
+    emplacement_nv7 = models.ManyToManyField('Degat',blank=True,related_name='emplacement_nv7')
+    emplacement_nv8 = models.ManyToManyField('Degat',blank=True,related_name='emplacement_nv8')
+    emplacement_nv9 = models.ManyToManyField('Degat',blank=True,related_name='emplacement_nv9')
+
+class DegatSortNiveaux(models.Model):
+    desc = models.TextField(null=True, blank=True)
+    niveau_0 = models.ManyToManyField('Degat',blank=True,related_name='niveau_0')
+    niveau_5 = models.ManyToManyField('Degat',blank=True,related_name='niveau_1')
+    niveau_11 = models.ManyToManyField('Degat',blank=True,related_name='niveau_2')
+    niveau_17 = models.ManyToManyField('Degat',blank=True,related_name='niveau_3')
+
+
+
+    
+class SoinSortEmplacements(models.Model):
+    desc = models.TextField(null=True, blank=True)
+    emplacement_nv0 = models.ManyToManyField('Soin',blank=True,related_name='emplacement_nv0')
+    emplacement_nv1 = models.ManyToManyField('Soin',blank=True,related_name='emplacement_nv1')
+    emplacement_nv2 = models.ManyToManyField('Soin',blank=True,related_name='emplacement_nv2')
+    emplacement_nv3 = models.ManyToManyField('Soin',blank=True,related_name='emplacement_nv3')
+    emplacement_nv4 = models.ManyToManyField('Soin',blank=True,related_name='emplacement_nv4')
+    emplacement_nv5 = models.ManyToManyField('Soin',blank=True,related_name='emplacement_nv5')
+    emplacement_nv6 = models.ManyToManyField('Soin',blank=True,related_name='emplacement_nv6')
+    emplacement_nv7 = models.ManyToManyField('Soin',blank=True,related_name='emplacement_nv7')
+    emplacement_nv8 = models.ManyToManyField('Soin',blank=True,related_name='emplacement_nv8')
+    emplacement_nv9 = models.ManyToManyField('Soin',blank=True,related_name='emplacement_nv9')
 
 class Monaie(models.Model):
     nom = models.CharField(primary_key=True, max_length=50)
--- src/data/serializers.py
+++ src/data/serializers.py
@@ -119,16 +119,53 @@ class SortListSerializers(BaseSerializers):
         model = Sort
         fields = ['index','nom','url']
 
+class DegatSortNiveauxSerializer(BaseSerializers):
+    niveau_0 = StringRelatedField(many=True)
+    niveau_5 = StringRelatedField(many=True)
+    niveau_11 = StringRelatedField(many=True)
+    niveau_17 = StringRelatedField(many=True)
+    class Meta:
+        model = DegatSortNiveaux
+        fields = ['desc','niveau_0','niveau_5','niveau_11','niveau_17',]
+
+class DegatSortEmplacementsSerializer(ModelSerializer):
+    emplacement_nv0 = StringRelatedField(many=True)
+    emplacement_nv1 = StringRelatedField(many=True)
+    emplacement_nv2 = StringRelatedField(many=True)
+    emplacement_nv3 = StringRelatedField(many=True)
+    emplacement_nv4 = StringRelatedField(many=True)
+    emplacement_nv5 = StringRelatedField(many=True)
+    emplacement_nv6 = StringRelatedField(many=True)
+    emplacement_nv7 = StringRelatedField(many=True)
+    emplacement_nv8 = StringRelatedField(many=True)
+    emplacement_nv9 = StringRelatedField(many=True)
+    class Meta:
+        model = DegatSortEmplacements
+        fields = ['desc','emplacement_nv0','emplacement_nv1','emplacement_nv2','emplacement_nv3','emplacement_nv4','emplacement_nv5','emplacement_nv6','emplacement_nv7','emplacement_nv8','emplacement_nv9',]
+
+class SoinSortEmplacementsSerializer(BaseSerializers):
+    emplacement_nv0 = StringRelatedField(many=True)
+    emplacement_nv1 = StringRelatedField(many=True)
+    emplacement_nv2 = StringRelatedField(many=True)
+    emplacement_nv3 = StringRelatedField(many=True)
+    emplacement_nv4 = StringRelatedField(many=True)
+    emplacement_nv5 = StringRelatedField(many=True)
+    emplacement_nv6 = StringRelatedField(many=True)
+    emplacement_nv7 = StringRelatedField(many=True)
+    emplacement_nv8 = StringRelatedField(many=True)
+    emplacement_nv9 = StringRelatedField(many=True)
+    class Meta:
+        model = SoinSortEmplacements
+        fields = ['desc','emplacement_nv0','emplacement_nv1','emplacement_nv2','emplacement_nv3','emplacement_nv4','emplacement_nv5','emplacement_nv6','emplacement_nv7','emplacement_nv8','emplacement_nv9',]
+
 class SortDetailSerializers(BaseSerializers):
     composantes = StringRelatedField(many=True)
-    temps_incantation = StringRelatedField()
-    duree = StringRelatedField()
-    portee = StringRelatedField()
-    cible = StringRelatedField()
-    degats = StringRelatedField(many=True)
+    degats_par_niveaux = DegatSortNiveauxSerializer()
+    degats_par_emplacement = DegatSortEmplacementsSerializer()
+    soins_par_emplacement = SoinSortEmplacementsSerializer()
     class Meta:
         model = Sort
-        fields = ['index','nom','desc','ecole','niveau','composantes','composante_desc','temps_incantation','concentration','duree','portee','cible','degats','jets_sauvegardes','jet_attaque','rituel','url']
+        fields = ['index','nom','desc','ecole','niveau','composantes','composante_desc','temps_incantation','concentration','duree','portee','jets_sauvegardes','jet_attaque','soins_par_emplacement','degats_par_emplacement','degats_par_niveaux','rituel','url']
 
 class OptionsSortSerializer(ModelSerializer):
     parmis = SortListSerializers(many=True)
