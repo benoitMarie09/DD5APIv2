37920ca4273b34b904e35a56983572c3f93be9d3
bouduben <bouduben@gmail.com>
2022-12-14 16:33:31 +0100

    requirement update

---
+++ DD5APIv2/static/accueil/js/accueil.js
@@ -0,0 +1,4 @@
+function interactiveCall() {
+  var content = document.getElementById("interactive").val();
+  console.log(content);
+}
---
+++ DD5APIv2/static/accueil/js/header.js
@@ -0,0 +1,6 @@
+
+function toggleMenu() {
+    const navbar = document.querySelector('.navbar')
+    console.log(navbar)
+    navbar.classList.toggle('show-nav');
+}
